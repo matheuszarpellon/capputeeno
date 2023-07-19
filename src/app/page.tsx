e37b82faeadd4a001bbbb0784f464b1ceca69d74
Matheus Zarpellon <mvzarpellon@gmail.com>
2023-07-19 00:28:27 -0300

    feat: products fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client"
 import { FilterBar } from './components/FilterBar'
+import { ProductsList } from './components/ProductsList'
 import styles from './page.module.css'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 export default function Home() {
+  const client = new QueryClient()
+
   return (
+    <QueryClientProvider client={client}>
     <main className={styles.main}>
       <FilterBar />
+      <ProductsList />
     </main>
+    </QueryClientProvider>
   )
 }
